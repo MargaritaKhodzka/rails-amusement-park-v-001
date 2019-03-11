ea2ad688ef718dedabde81465deb2ddb5e64293c
MargaritaKhodzka <mkhodzka@gmail.com>
2019-03-10 22:32:09 -0400

    added controllers and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+  end
+
+  def create
+    @user = User.create(user_params)
+    return redirect_to controller: 'users', action: 'new' unless @user.save
+    session[:user_id] = @user.id
+    redirect_to controller: 'welcome', action: 'home'
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :height, :tickets, :happiness, :nausea, :admin)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  
+  root 'welcome#home'
+
+  get '/signin', to: 'sessions#new'
+  post '/sessions/create', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+
+  post '/rides/new', to: 'rides#new'
+
+  resources :users
+  resources :attractions
 end
